3682097700b6faefc19252ac6e605b723b5a629a
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-07-27 01:22:03 +0630

    feat: config

---
+++ DotNet8WebApi.SupabaseSample/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿// Global using directives
+
+global using DotNet8WebApi.SupabaseSample.Dtos;
+global using DotNet8WebApi.SupabaseSample.Repositories;
+global using DotNet8WebApi.SupabaseSample.Services;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
+global using Supabase.Postgrest;
