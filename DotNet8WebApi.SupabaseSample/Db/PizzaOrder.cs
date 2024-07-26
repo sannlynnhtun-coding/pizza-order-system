83437779f63bbd4ec376e9d8f711dc2b787ac457
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-07-27 01:15:08 +0630

    feat: add table from supabase

---
+++ DotNet8WebApi.SupabaseSample/Db/PizzaOrder.cs
@@ -0,0 +1,23 @@
+﻿using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+
+namespace DotNet8WebApi.SupabaseSample.Db;
+
+[Table("pizza_orders")]
+public class PizzaOrder : BaseModel
+{
+    [PrimaryKey("id", false)]
+    public int Id { get; set; }
+
+    [Column("customer_name")]
+    public string CustomerName { get; set; }
+
+    [Column("pizza_type")]
+    public string PizzaType { get; set; }
+
+    [Column("quantity")]
+    public int Quantity { get; set; }
+
+    [Column("status")]
+    public string Status { get; set; }
+}
