d273c147d866a03c8900411ff9f45cb70206c01d
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-07-27 01:16:01 +0630

    feat: repositories

---
+++ DotNet8WebApi.SupabaseSample/Repositories/PizzaOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using DotNet8WebApi.SupabaseSample.Db;
+using DotNet8WebApi.SupabaseSample.Services;
+
+namespace DotNet8WebApi.SupabaseSample.Repositories;
+
+public class PizzaOrderRepository : Repository<PizzaOrder>
+{
+    public PizzaOrderRepository(SupabaseService supabaseService) : base(supabaseService)
+    {
+    }
+}
---
+++ DotNet8WebApi.SupabaseSample/Repositories/Repository.cs
@@ -0,0 +1,47 @@
+﻿using DotNet8WebApi.SupabaseSample.Services;
+using Supabase.Postgrest.Models;
+
+namespace DotNet8WebApi.SupabaseSample.Repositories;
+
+public class Repository<T> where T : BaseModel, new()
+{
+    private readonly SupabaseService _supabaseService;
+
+    public Repository(SupabaseService supabaseService)
+    {
+        _supabaseService = supabaseService;
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        var client = _supabaseService.GetClient();
+        var response = await client.From<T>().Get();
+        return response.Models;
+    }
+
+    public async Task<T> GetByIdAsync(int id)
+    {
+        var client = _supabaseService.GetClient();
+        var response = await client.From<T>().Filter("id", Constants.Operator.Equals, id).Get();
+        return response.Models.FirstOrDefault();
+    }
+
+    public async Task<IEnumerable<T>> InsertAsync(List<T> entities)
+    {
+        var client = _supabaseService.GetClient();
+        var response = await client.From<T>().Insert(entities);
+        return response.Models;
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        var client = _supabaseService.GetClient();
+        await client.From<T>().Update(entity);
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var client = _supabaseService.GetClient();
+        await client.From<T>().Filter("id", Constants.Operator.Equals, id).Delete();
+    }
+}
