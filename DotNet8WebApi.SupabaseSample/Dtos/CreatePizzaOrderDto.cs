9601d5c77f8ed07ed23647343d7ad26bffc7dc5e
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-07-27 01:15:24 +0630

    feat: dtos

---
+++ DotNet8WebApi.SupabaseSample/Dtos/CreatePizzaOrderDto.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNet8WebApi.SupabaseSample.Dtos;
+
+public class CreatePizzaOrderDto
+{
+    public string CustomerName { get; set; }
+    public string PizzaType { get; set; }
+    public int Quantity { get; set; }
+    public string Status { get; set; }
+}
---
+++ DotNet8WebApi.SupabaseSample/Dtos/PizzaOrderDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNet8WebApi.SupabaseSample.Dtos;
+
+public class PizzaOrderDto
+{
+    public int Id { get; set; }
+    public string CustomerName { get; set; }
+    public string PizzaType { get; set; }
+    public int Quantity { get; set; }
+    public string Status { get; set; }
+}
