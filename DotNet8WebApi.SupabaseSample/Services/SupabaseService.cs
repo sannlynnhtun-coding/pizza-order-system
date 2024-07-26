6e8422d18d9b3d32e732d2b98c9bc56dc8802e4b
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-07-27 01:14:37 +0630

    feat: SupabaseService.cs

---
+++ DotNet8WebApi.SupabaseSample/Services/SupabaseService.cs
@@ -0,0 +1,25 @@
+﻿using Client = Supabase.Client;
+
+namespace DotNet8WebApi.SupabaseSample.Services;
+
+public class SupabaseService
+{
+    private readonly Client _client;
+
+    public SupabaseService(IConfiguration configuration)
+    {
+        var url = configuration["Supabase:Url"];
+        var key = configuration["Supabase:Key"];
+        _client = new Client(url, key);
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _client.InitializeAsync();
+    }
+
+    public Client GetClient()
+    {
+        return _client;
+    }
+}
