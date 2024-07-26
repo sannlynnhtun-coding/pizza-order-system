12e71c0edd692aed043adeb119acfd28bce468b3
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-07-27 01:21:49 +0630

    feat: add env

--- DotNet8WebApi.SupabaseSample/Program.cs
+++ DotNet8WebApi.SupabaseSample/Program.cs
@@ -1,6 +1,11 @@
+DotNetEnv.Env.Load();
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<SupabaseService>();
+builder.Services.AddScoped<PizzaOrderRepository>();
+builder.Services.AddScoped(typeof(Repository<>));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
