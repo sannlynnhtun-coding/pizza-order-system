43184490f22510dc7e020cd231ceada5789f16e2
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-07-27 01:15:38 +0630

    feat: mapping

---
+++ DotNet8WebApi.SupabaseSample/Mapping/PizzaOrderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using DotNet8WebApi.SupabaseSample.Db;
+
+namespace DotNet8WebApi.SupabaseSample.Mapping;
+
+public static class PizzaOrderExtensions
+{
+    public static PizzaOrderDto ToDto(this PizzaOrder pizzaOrder)
+    {
+        return new PizzaOrderDto
+        {
+            Id = pizzaOrder.Id,
+            CustomerName = pizzaOrder.CustomerName,
+            PizzaType = pizzaOrder.PizzaType,
+            Quantity = pizzaOrder.Quantity,
+            Status = pizzaOrder.Status
+        };
+    }
+}
